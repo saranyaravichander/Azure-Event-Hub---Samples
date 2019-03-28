9073ddc8dc5cef9f9f27a7fa6890c2f21e5b7c08
saranyaravichander <saranyaravichander@live.com>
2019-03-28 17:26:10 -0400

    Event hub ingest and read samples

---
+++ ReadFromEventHub/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ReadFromEventHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eventHubConnectionString = "Endpoint=sb://novanteh.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=/X/1/IdRHCqqugRdSieg40PYpGwbCKQIQsmJRJSAGjM=";
+            string eventHubName = "outgoingmsg";
+            string storageAccountName = "novanteventproc9648";
+            string storageAccountKey = "Vv3vWTAHj/MxcBg+zNQURKE2v0NniFnxtb6Cg7jR8/THMrPS+QCiMudJS5paBXb9iFVScRdN7rX33P6oxWu5hQ==";
+            string storageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", storageAccountName, storageAccountKey);
+
+            string eventProcessorHostName = Guid.NewGuid().ToString();
+            EventProcessorHost eventProcessorHost = new EventProcessorHost(
+                eventProcessorHostName, 
+                eventHubName, 
+                EventHubConsumerGroup.DefaultGroupName, 
+                eventHubConnectionString, 
+                storageConnectionString);
+            Console.WriteLine("Registering EventProcessor...");
+            var options = new EventProcessorOptions();
+            options.ExceptionReceived += (sender, e) => { Console.WriteLine(e.Exception); };
+            eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>(options).Wait();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+            eventProcessorHost.UnregisterEventProcessorAsync().Wait();
+        }
+    }
+}
